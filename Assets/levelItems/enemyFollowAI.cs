2a47b9c84bbd40c8d12c2d528ae3612e152dc1ac
Michael Nicoletti <mike.e.nicoletti@gmail.com>
2015-06-03 19:18:24 -0400

    AI Development
    Started working on AI development

---
+++ Assets/levelItems/enemyFollowAI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyFollowAI : MonoBehaviour {
+
+    public GameObject targetTwo;
+
+    private Vector3 initPos;
+
+    public GameObject player;
+
+    private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () 
+    {
+
+        agent = GetComponent<NavMeshAgent>();
+
+        initPos = transform.position;
+	
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+
+        agent.SetDestination(targetTwo.transform.position);
+
+
+
+    }
+
+}
